77b9981025c80c6ba762a567774c681dae7da793
Ranviee0 <neodyte@gmail.com>
2025-06-26 15:09:34 +0700

    directly get ID data from API

--- main.py
+++ main.py
@@ -7,6 +7,9 @@
 import sys
 from ThaiCIDHelper  import *
 from DataThaiCID    import *
+from fastapi import FastAPI
+
+app = FastAPI()
 
 # สร้าง Instance Class ThaiCIDHelper
 reader = ThaiCIDHelper(APDU_SELECT,APDU_THAI_CARD)
@@ -14,16 +17,35 @@ reader = ThaiCIDHelper(APDU_SELECT,APDU_THAI_CARD)
 # Connect SMC
 connection, status = reader.connectReader(0)
 
-if status:
-    data = reader.readData()
-    print("📄 Thai Description Text:")
-    print(data["thaiText"])
+# if status:
+#     data = reader.readData()
+#     print("📄 Thai Description Text:")
+#     print(data["thaiText"])
     
-    print("\n🧾 JSON Text:")
-    print(data["jsonText"])
+#     print("\n🧾 JSON Text:")
+#     print(data["jsonText"])
     
-    print("\n🔑 JSON as Dictionary:")
-    print(data["jsonData"])  # You can also access specific fields: data["jsonData"]["fname"]
-else:
-    print(f'❌ Error: {reader.lastError}')
-    sys.exit(1)
+#     print("\n🔑 JSON as Dictionary:")
+#     print(data["jsonData"])  # You can also access specific fields: data["jsonData"]["fname"]
+# else:
+#     print(f'❌ Error: {reader.lastError}')
+#     sys.exit(1)
+
+
+@app.get("/read-cid")
+def read_citizen_data():
+    reader = ThaiCIDHelper()
+
+    if not reader.cardReaderList:
+        return {"error": "No card reader found."}
+
+    connection, status = reader.connectReader(0)
+
+    if status and reader.cardReader is not None:
+        try:
+            data = reader.readData(readPhoto=False)
+            return data["jsonData"]
+        except Exception as e:
+            return {"error": f"Reading failed: {str(e)}"}
+    else:
+        return {"error": f"Connection failed: {reader.lastError}"}
